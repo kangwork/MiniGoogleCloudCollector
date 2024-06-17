32d1f2764c71c7334253698dd1d7df110d29da91
kangwork <irene.kang@mail.utoronto.ca>
2024-06-17 10:38:36 -0400

    [Style] Removed an unused dict(all_credential_fields)

--- src/utils/credentials.py
+++ src/utils/credentials.py
@@ -30,20 +30,6 @@ def _get_credentials_from_env() -> Credentials:
     return credentials
 
 
-all_fields = [
-    "type",
-    "project_id",
-    "private_key_id",
-    "private_key",
-    "client_email",
-    "client_id",
-    "auth_uri",
-    "token_uri",
-    "auth_provider_x509_cert_url",
-    "client_x509_cert_url",
-    "universe_domain",
-]
-
 required_fields = {
     "private_key": "",
     "client_email": "",
