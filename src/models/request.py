89854628c80f1abab3356a1511766ce6bba9b756
kangwork <irene.kang@mail.utoronto.ca>
2024-06-19 10:12:33 -0400

    [Style] Removed unnecessary comments

--- src/models/request.py
+++ src/models/request.py
@@ -3,15 +3,6 @@ from typing import Dict
 from utils.credentials import get_credentials
 from abc import ABC
 
-# class SecretData(BaseModel): I don't know if it is better to define this class, so I will leave it till the meeting today.
-#     private_key: str
-#     client_email: str
-#     project_id: str = None
-
-#     @property
-#     def credentials(self):
-#         return get_credentials(self.dict())
-
 class ResourceAccessRequest(BaseModel, ABC):
     secret_data: Dict[str, str]
 
@@ -25,8 +16,7 @@ class ListResourcesRequest(ResourceAccessRequest):
 
 
 class GetResourceRequest(ResourceAccessRequest):
-    param: str  # role_id, bucket_name, etc
-
+    param: str
 
 class GetCEInstanceRequest(ResourceAccessRequest):
     zone: str
